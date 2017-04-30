5ca9c13f208d745ce9cfbeea6b2bc3d1389db543
Dario Vranjković <vranja33@gmail.com>
2017-04-30 14:10:32 +0200

    Add project files.

---
+++ Gigs1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gigs1.MvcApplication" Language="C#" %>
---
+++ Gigs1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gigs1.Startup))]
+namespace Gigs1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
