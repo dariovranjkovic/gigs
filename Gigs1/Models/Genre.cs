cfb0f1b0a4d95e09e26310cfb0bd0ce16057b50a
Dario Vranjković <vranja33@gmail.com>
2017-04-30 15:05:36 +0200

    Override conventions for Gigs and Genres

--- Gigs1/Models/Genre.cs
+++ Gigs1/Models/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,10 @@ namespace Gigs1.Models
 {
     public class Genre
     {
-            public byte Id { get; set; }
-            public string Name { get; set; }
+        public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
 }
--- Gigs1/Models/Gig.cs
+++ Gigs1/Models/Gig.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Gigs1.Models
 {
     public class Gig
     {
         public int Id { get; set; }
+
+        [Required]
         public ApplicationUser Artist { get; set; }
+
         public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Venue { get; set; }
+
+        [Required]
         public Genre Genre { get; set; }
     }
 }
