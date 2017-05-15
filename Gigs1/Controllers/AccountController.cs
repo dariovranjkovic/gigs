e0b089a4f8af1a51ad0dd98e9f99f69042d2a0a0
Dario Vranjković <vranja33@gmail.com>
2017-05-15 23:34:41 +0200

    Add name to artist

--- Gigs1/Controllers/AccountController.cs
+++ Gigs1/Controllers/AccountController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
+﻿using Gigs1.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using Gigs1.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Gigs1.Controllers
 {
@@ -151,7 +148,7 @@ namespace Gigs1.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
