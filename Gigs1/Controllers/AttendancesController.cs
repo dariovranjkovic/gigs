16a07aa354be6a6d7644aa7e4ccba83d719568af
Dario Vranjković <vranja33@gmail.com>
2017-05-18 23:59:37 +0200

    Add WebApi (Attendances)

---
+++ Gigs1/Controllers/AttendancesController.cs
@@ -0,0 +1,31 @@
+﻿using Gigs1.Models;
+using Microsoft.AspNet.Identity;
+using System.Web.Http;
+
+namespace Gigs1.Controllers
+{
+
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var attendence = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _context.Attendances.Add(attendence);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
